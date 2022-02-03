6093c3b2e1cfd649f9b831e213ca03998a4dd0af
jum0 <wnsah052@naver.com>
2022-02-04 00:29:24 +0900

    chore: 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>hello whynot-here</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,47 @@
+import { css, Global } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${emotionNormalize}
+
+      * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        border: 0;
+      }
+
+      html,
+      body,
+      #root {
+        min-height: 100%;
+        height: 100%;
+        font-family: Helvetica, Arial, sans-serif;
+      }
+
+      li {
+        list-style: none;
+      }
+
+      input {
+        font-weight: 300;
+      }
+
+      // input 숫자 화살표 삭제
+      /* Chrome, Safari, Edge, Opera */
+      input::-webkit-outer-spin-button,
+      input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+      }
+
+      /* Firefox */
+      input[type='number'] {
+        -moz-appearance: textfield;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
