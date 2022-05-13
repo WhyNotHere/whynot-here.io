4f15eab7b6ad4349a49d235b37dd6420650ad19e
jum0 <wnsah052@naver.com>
2022-05-13 12:08:40 +0900

    chore: react-query 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 import { theme } from './styles/theme';
 import GlobalStyle from './GlobalStyle';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>,
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>,
   document.getElementById('root'),
 );
